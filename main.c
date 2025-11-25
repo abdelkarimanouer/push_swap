02998a7f9275b74684e6d057989f19629c3a61c5
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-11-25 19:57:44 +0100

    add file

--- main.c
+++ main.c
@@ -25,6 +25,5 @@ int	main(int argc, char **argv)
 			i++;
 		}
 	}
-
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -4,6 +4,14 @@
 # include <stdlib.h>
 # include "ft_printf/ft_printf.h"
 
+typedef struct st_linked_list
+{
+	int	data;
+	struct st_linked_list *prev;
+	struct st_linked_list *next;
+}	linked_list;
+
+
 int	ft_atoi(const char *str, int *failed);
 
 #endif
