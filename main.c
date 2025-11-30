ff79bb3a26b5d5fe16b8f2cd1d9a52d4b7ce3e39
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-11-30 02:59:44 +0100

    edit main file

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 10:23:19 by aanouer           #+#    #+#             */
-/*   Updated: 2025/11/29 20:54:00 by aanouer          ###   ########.fr       */
+/*   Updated: 2025/11/30 02:54:32 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static int	start_the_code(int argc, char **argv)
 {
 	t_list	*a;
 	t_list	*b;
@@ -21,22 +21,28 @@ int	main(int argc, char **argv)
 	a = NULL;
 	b = NULL;
 	i = 1;
-	if (argc > 1)
+	if (argc == 1)
+		return (0);
+	if (argc == 2)
 	{
-		if (argc == 2)
+		if (call_split(argv[i], &a) == -1)
+			return (write(2, "Error\n", 6), 1);
+	}
+	else
+	{
+		while (i < argc)
 		{
-			if (call_split(argv[i], &a) == -1)
+			if (call_atoi(argv[i], &a) == -1)
 				return (write(2, "Error\n", 6), 1);
-		}
-		else
-		{
-			while (i < argc)
-			{
-				if (call_atoi(argv[i], &a) == -1)
-					return (write(2, "Error\n", 6), 1);
-				i++;
-			}
+			i++;
 		}
 	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (start_the_code(argc, argv))
+		return (1);
 	return (0);
 }
