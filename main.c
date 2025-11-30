afceb3dc3b9aff22bf3c7bc63825850d55416675
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-11-30 03:06:05 +0100

    edit main file

--- main.c
+++ main.c
@@ -6,33 +6,29 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 10:23:19 by aanouer           #+#    #+#             */
-/*   Updated: 2025/11/30 02:54:32 by aanouer          ###   ########.fr       */
+/*   Updated: 2025/11/30 03:05:11 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	start_the_code(int argc, char **argv)
+static int	start_and_fill_a(int argc, char **argv, t_list **a)
 {
-	t_list	*a;
-	t_list	*b;
 	int		i;
 
-	a = NULL;
-	b = NULL;
 	i = 1;
 	if (argc == 1)
 		return (0);
 	if (argc == 2)
 	{
-		if (call_split(argv[i], &a) == -1)
+		if (call_split(argv[i], a) == -1)
 			return (write(2, "Error\n", 6), 1);
 	}
 	else
 	{
 		while (i < argc)
 		{
-			if (call_atoi(argv[i], &a) == -1)
+			if (call_atoi(argv[i], a) == -1)
 				return (write(2, "Error\n", 6), 1);
 			i++;
 		}
@@ -42,7 +38,15 @@ static int	start_the_code(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-	if (start_the_code(argc, argv))
+	t_list	*a;
+	t_list	*b;
+
+	a = NULL;
+	b = NULL;
+	if (start_and_fill_a(argc, argv, &a))
 		return (1);
+
+	free_stack(&a);
+	free_stack(&b);	
 	return (0);
 }
