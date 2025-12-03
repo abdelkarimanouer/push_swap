4b413f3d9bcf6c855ead5da44bc176c32fde3f1f
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-03 21:56:30 +0100

    add file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 10:23:19 by aanouer           #+#    #+#             */
-/*   Updated: 2025/12/02 23:32:23 by aanouer          ###   ########.fr       */
+/*   Updated: 2025/12/03 21:56:20 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ static int	start_and_fill_a(int argc, char **argv, t_list **a)
 	int		i;
 
 	i = 1;
-	if (argc == 1)
-		return (0);
 	if (argc == 2)
 	{
 		if (call_split(argv[i], a) == -1)
@@ -36,30 +34,52 @@ static int	start_and_fill_a(int argc, char **argv, t_list **a)
 	return (0);
 }
 
-static int	handle_3_case(t_list **a, t_list *max)
+static void	handle_3_case(t_list **a, t_list *max)
 {
 	t_list	*tmp;
 
 	tmp = *a;
-
-	if (!check_if_stack_sort(*a))
+	while (tmp && tmp != max)
+		tmp = tmp->next;
+	if (tmp == max)
 	{
-		max = find_max_node_by_rank(*a);
+		if (max->prev)
+			max->prev->next = max->next;
+		else
+			*a = max->next;
+		if (max->next)
+			max->next->prev = max->prev;
 	}
+	tmp = *a;
+	while (tmp->next)
+		tmp = tmp->next;
+	if (tmp != max)
+	{
+		tmp->next = max;
+		max->prev = tmp;
+		max->next = NULL;
+	}
+	if ((*a)->data > (*a)->next->data)
+		sa(a);
 }
 
 static int	handle_2_3_case(t_list **a)
 {
 	t_list	*max;
 
-	if (count_stack(a) == 2)
+	if (count_stack(*a) == 2)
 	{
-		if (!check_if_stack_sort(*a))
-			return (sa(&a), 1);
+		sa(a);
+		return (1);
 	}
-	//if (count_stack(a) == 2)
-	//	handle_3_case(&a, max);
-	return (0);
+	max = find_max_node_by_rank(*a);
+	if (count_stack(*a) == 3)
+	{
+		handle_3_case(a, max);
+		return (1);
+	}
+	else
+		return (0);
 }
 
 int	main(int argc, char **argv)
@@ -69,12 +89,19 @@ int	main(int argc, char **argv)
 
 	a = NULL;
 	b = NULL;
-	if (start_and_fill_a(argc, argv, &a))
-		return (free_stack(&a), free_stack(&b), write(2, "Error\n", 6), 1);
-	get_ranks_of_nodes(&a);
-	set_index_of_nodes(&a);
-		
-	free_stack(&a);
-	free_stack(&b);
+	if (argc > 1)
+	{
+		if (start_and_fill_a(argc, argv, &a))
+			return (free_stack(&a), free_stack(&b), write(2, "Error\n", 6), 1);
+		get_ranks_of_nodes(&a);
+		set_index_of_nodes(&a);
+		if (check_if_stack_sort(a))
+			return (free_stack(&a), free_stack(&b), 0);
+		if (!handle_2_3_case(&a))
+			return (1);
+			
+		free_stack(&a);
+		free_stack(&b);
+	}
 	return (0);
 }
