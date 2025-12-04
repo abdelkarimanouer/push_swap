337463b451c5ec8737d24d90c0426ec3d05bf15d
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-04 15:17:38 +0100

    add file

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 11:24:24 by aanouer           #+#    #+#             */
-/*   Updated: 2025/12/04 14:45:25 by aanouer          ###   ########.fr       */
+/*   Updated: 2025/12/04 15:16:29 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,10 @@ int	check_if_stack_sort(t_list *a)
 	return (1);
 }
 
-void	sort_3(t_list **a)
+int	sort_2_3_nodes(t_list **a)
 {
 	t_list	*max;
 
-	max = find_max_node_by_rank(*a);
-	if (max == *a)
-		ra(a);
-	else if (max == (*a)->next)
-		rra(a);
-	if ((*a)->data > (*a)->next->data)
-		sa(a);
-}
-
-int	sort_2_3_nodes(t_list **a)
-{
 	if (count_stack(*a) == 2)
 	{
 		sa(a);
@@ -46,7 +35,13 @@ int	sort_2_3_nodes(t_list **a)
 	}
 	if (count_stack(*a) == 3)
 	{
-		sort_3(a);
+		max = find_max_node_by_rank(*a);
+		if (max == *a)
+			ra(a);
+		else if (max == (*a)->next)
+			rra(a);
+		if ((*a)->data > (*a)->next->data)
+			sa(a);
 		return (1);
 	}
 	else
